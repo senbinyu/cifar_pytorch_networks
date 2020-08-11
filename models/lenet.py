1a11632c8a5e071efa1398ea7996ae05801d6e2d
senbinyu <42667259+senbinyu@users.noreply.github.com>
2020-08-11 22:28:37 +0200

    Create lenet.py

---
+++ models/lenet.py
@@ -0,0 +1,34 @@
+# lenet. The parameters come from the 'https://towardsdatascience.com/understanding-and-implementing-lenet-5-cnn-architecture-deep-learning-a2d531ebc342'
+# Input size is fixed to be 32 * 32, if want to use other sizes, the parameters in __init__(self) should be modified too.
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)  # here 3, input channel, 6, output channel, 5, kernel size
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*5*5, 120) # here 16 output channel in the above conv, 5 kernel size, above conv
+        self.fc2   = nn.Linear(120, 84)
+        self.fc3   = nn.Linear(84, 10)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        out = self.fc3(out)
+        return out
+		
+def test():
+    net = LeNet()
+    x = torch.randn(2,3,32,32)
+    y = net(x)
+    print(y.size())
+
+# use test to check the model
+# test() 
